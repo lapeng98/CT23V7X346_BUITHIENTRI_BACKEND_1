82969d881739e2db83adb1c9e097c1263a3ab182
tree <salinger7313@gmail.com>
2025-04-19 09:12:13 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
-// start server
-const PORT = config.app.port;
+async function startServer() {
+    try {
+        // Kết nối MongoDB
+        await MongoDB.connect(config.db.uri);
+        console.log("Connected to the database!");
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
+        // Khởi động HTTP server
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log("Cannot connect to the database!", error);
+        process.exit();
+    }
+}
+
+startServer();
